c1454f35e00bb73295e1ec3b54ce629ff129b46d
ishanpathak98 <95092310+ishanpathak98@users.noreply.github.com>
2025-06-19 13:45:14 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NoteApp from './components/NoteApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="app-title">Ishan's Customized Notes App</h1>
+      <NoteApp />
+    </div>
+  );
+}
+
+export default App;
